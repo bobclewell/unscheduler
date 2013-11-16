2b19a7888d8b68ad4c8ac04119e66760934d49f0
Bob Clewell <bobclewell@gmail.com>
2013-11-15 23:40:29 -0500

    Adding room form basics. Also removed the default hello world stuff.

--- unscheduler.js
+++ unscheduler.js
@@ -2,21 +2,25 @@ Times = new Meteor.Collection("times");
 Rooms = new Meteor.Collection("rooms");
 
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to unscheduler.";
-  };
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
-
   Template.room_list.rooms = function () {
     return Rooms.find({}, {sort: {name: 1}});
   };
+
+  Template.room_form.events({
+    'click #add-room' : function () {
+      roomName = $('#room-name').val();
+      Rooms.insert({name: roomName});
+      $('#room-name').val('');
+    },
+
+    'keypress input#room-name': function (evt) {
+      if (evt.which === 13) {
+        roomName = $('#room-name').val();
+        Rooms.insert({name: roomName});
+        $('#room-name').val('');
+      }
+    }
+  });
 }
 
 if (Meteor.isServer) {
